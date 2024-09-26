813fd6f2f2a0c0f9967f72012fdcf035dc3a8c85
JyotiGupta <106060822+Jyoangel@users.noreply.github.com>
2024-09-26 14:20:28 +0530

    Update layout.jsx

--- src/app/teacherspanel/layout.jsx
+++ src/app/teacherspanel/layout.jsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       return;
     }
 
-    const userRoles = user[`${AUTH0_BASE_URL}/roles`];
+    const userRoles = user[`${process.env.AUTH0_BASE_URL}/roles`];
     if (!userRoles || !userRoles.includes('Teacher')) {
       router.push('/'); // Redirect if not a teacher
       return;
