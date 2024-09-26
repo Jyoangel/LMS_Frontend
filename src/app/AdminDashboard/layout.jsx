09e1dca73d26e33b80a137918364a9fd16d648c2
JyotiGupta <106060822+Jyoangel@users.noreply.github.com>
2024-09-26 12:59:06 +0530

    Update layout.jsx

--- src/app/AdminDashboard/layout.jsx
+++ src/app/AdminDashboard/layout.jsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       return;
     }
 
-    const userRoles = user['https://localhost:3000/roles'];
+    const userRoles = user[`${AUTH0_BASE_URL}/roles`];
     if (!userRoles || !userRoles.includes('Admin')) {
       router.push('/'); // Redirect if not admin
       return;
